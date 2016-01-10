55c65402634cc95641fe752c22809c21b6ba7b9f
Matt Bostock <matt@mattbostock.com>
2016-01-10 18:42:51 +0000

    Add TestFunctionalArrayList
    Add `TestFunctionalArrayList`, which extends `ArrayList` and implements
    the `FunctionalList` interface.

---
+++ FunctionalArrayList.java
@@ -0,0 +1,29 @@
+/**
+ * Extend {@see ArrayList} by implementing the {@see FunctionalList} interface.
+ *
+ * @author Matt Bostock
+ */
+public class FunctionalArrayList extends ArrayList implements FunctionalList {
+  public ReturnObject head() {
+    if (this.list[0] == null) {
+      return new ReturnObjectImpl(ErrorMessage.EMPTY_STRUCTURE);
+    }
+
+    return new ReturnObjectImpl(this.list[0]);
+  }
+
+  public FunctionalList rest() {
+    FunctionalArrayList tmpList = new FunctionalArrayList();
+
+    if (this.size == 0) {
+      tmpList.list = this.list;
+      return tmpList;
+    }
+
+    for (int i = 1; i < this.size; i++) {
+      tmpList.add(this.list[i]);
+    }
+
+    return tmpList;
+  }
+}
---
+++ TestFunctionalArrayList.java
@@ -0,0 +1,44 @@
+public class TestFunctionalArrayList {
+  public static void main(String[] args) {
+    FunctionalArrayList l = new FunctionalArrayList();
+
+    TestHelpers.assertEquals(l.size(), 0);
+    TestHelpers.assertEquals(l.isEmpty(), true);
+    TestHelpers.assertEquals(l.head().getError(), ErrorMessage.EMPTY_STRUCTURE);
+    TestHelpers.assertEquals(l.rest().size(), 0);
+
+    l.add("foo");
+    TestHelpers.assertEquals(l.isEmpty(), false);
+    TestHelpers.assertEquals(l.size(), 1);
+    TestHelpers.assertEquals(l.head().getReturnValue(), "foo");
+    TestHelpers.assertEquals(l.rest().size(), 0);
+
+    l.add("bar");
+    TestHelpers.assertEquals(l.isEmpty(), false);
+    TestHelpers.assertEquals(l.size(), 2);
+    TestHelpers.assertEquals(l.head().getReturnValue(), "foo");
+    TestHelpers.assertEquals(l.rest().size(), 1);
+
+    l.add(1, "baz");
+    TestHelpers.assertEquals(l.isEmpty(), false);
+    TestHelpers.assertEquals(l.size(), 3);
+    TestHelpers.assertEquals(l.get(0).getReturnValue(), "foo");
+    TestHelpers.assertEquals(l.get(1).getReturnValue(), "baz");
+    TestHelpers.assertEquals(l.get(2).getReturnValue(), "bar");
+    TestHelpers.assertEquals(l.head().getReturnValue(), "foo");
+    TestHelpers.assertEquals(l.rest().size(), 2);
+
+    l.remove(1);
+    TestHelpers.assertEquals(l.isEmpty(), false);
+    TestHelpers.assertEquals(l.size(), 2);
+    TestHelpers.assertEquals(l.get(0).getReturnValue(), "foo");
+    TestHelpers.assertEquals(l.get(1).getReturnValue(), "bar");
+    TestHelpers.assertEquals(l.head().getReturnValue(), "foo");
+    TestHelpers.assertEquals(l.rest().size(), 1);
+
+    ReturnObject ret = l.get(2);
+    TestHelpers.assertNull(ret.getReturnValue());
+    TestHelpers.assertEquals(ret.getError(), ErrorMessage.INDEX_OUT_OF_BOUNDS);
+    TestHelpers.assertEquals(ret.hasError(), true);
+  }
+}
